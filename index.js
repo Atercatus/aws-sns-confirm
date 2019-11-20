7947d33333e6f51b03c53dbd1a4225f0e289c7a9
Atercatus <myleelee2053@naver.com>
2019-11-20 23:15:48 +0900

    push

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const AWS = require("aws-sdk");
+const bodyParser = require("body-parser");
 
 const PORT = 8080;
 
 const app = express();
+
+console.log(process.env.ACCESS_KEY_ID)
 
 AWS.config.update({
   accessKeyId: process.env.ACCESS_KEY_ID,
@@ -14,8 +17,17 @@ AWS.config.update({
 
 const sns = new AWS.SNS();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use((req,res,next)=>{
+  if(req.get('x-amz-sns-message-type')){
+    req.headers['content-type'] = 'application/json';
+  }
+  next();
+});
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 
 app.post("/", (req, res) => {
   const options = {
@@ -23,7 +35,9 @@ app.post("/", (req, res) => {
     Token: req.body.Token
   };
 
+  console.log(options);
   sns.confirmSubscription(options);
+  res.send(); 
 });
 
 app.listen(PORT, () => {
